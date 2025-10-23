63285327a38860dc600e59dc885c3521b1033177
Okan Tümüklü <117488504+okansecurity@users.noreply.github.com>
2025-10-24 00:43:42 +0300

    Refactor string input and implement dynamic array
    Updated string handling and added dynamic array manipulation.

--- test.c
+++ test.c
@@ -1,10 +1,34 @@
 #include "hyper_core.h"
-#include "string.h"
 
 int main(void){
 	
-	string isim = get_string("isim gir:");
-	printf("isminizin bellekteki tutulduğu yer %ld",strlen(isim));
-	free(isim);
+	string name = get_string("Enter name:");
+	printf("you name: %s \n",name);
+	
+	printf("\n");
+	
+	int* arry = (int *) malloc(sizeof(int)*5);
+	
+	for(int i = 0; i < 5; i++){
+		arry[i] = i;
+		}
+	
+	printf("before addelement\n");
+	for(int i = 0; i < 5; i++){
+		printf("%d-",arry[i]);
+		}
+	
+	for(int i = 5; i < 10; i++){
+		Int_AddElement(&arry,i,i+1);
+		}
+	
+	printf("\nafeter addelement\n");
+	for(int i = 0; i < 10; i++){
+		printf("%d-",arry[i]);
+		}
+	
+	printf("\n");
 
 	}
+	
+	
