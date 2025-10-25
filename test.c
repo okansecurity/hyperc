da623649cf68eb6882f38434e2db0b3c393d0bba
Okan Tümüklü <117488504+okansecurity@users.noreply.github.com>
2025-10-25 19:10:25 +0300

    Refactor string input and array handling
    Refactor and uncomment code for array manipulation and string input.

--- test.c
+++ test.c
@@ -1,35 +1,116 @@
 #include "hyper_core.h"
 
+
 int main(void){
 	
-	string name = get_string("Enter name:");
-	printf("you name: %s \n",name);
+	/* For get string */
+	/*
+	string name = get_string("Plaese enther name:");
+	printf("You name:%s",name);
 	
 	printf("\n");
+	*/
 	
-	int* arry = (int *) malloc(sizeof(int)*5);
+	// add element for array  
+	/*
+	int size = 5;
+	int* arry = malloc(sizeof(int)*size);
 	
-	for(int i = 0; i < 5; i++){
-		arry[i] = i;
+	for(int i = 0; i < size; i++){
+		arry[i] = i+1;
 		}
-	
-	printf("before addelement\n");
-	for(int i = 0; i < 5; i++){
+		
+	printf("Before add funcs\n");
+	for(int i = 0; i < size; i++){
 		printf("%d-",arry[i]);
 		}
-
-	/* add element */
-	for(int i = 5; i < 10; i++){
-		Int_AddElement(&arry,i,i+1);
+		
+	
+	Int_AddElement(&arry,size,10,ARRY_END);
+	
+	printf("\n");
+	printf("after add funcs\n");
+	for(int i = 0; i < size+1; i++){
+		printf("%d-",arry[i]);
+		}
+	*/
+	
+		
+	/* to add more elements: example size 5 -> 50 */
+	
+	/*
+    int size = 5;
+	int* arry = malloc(sizeof(int)*size);
+	
+	for(int i = 0; i < size; i++){
+		arry[i] = i+1;
 		}
 	
-	printf("\nafeter addelement\n");
-	for(int i = 0; i < 10; i++){
+	printf("Before add funcs\n");
+	for(int i = 0; i < size; i++){
 		printf("%d-",arry[i]);
+		}
+	
+	for(int i = size; i < 50;i++){
+		Int_AddElement(&arry,size,i,ARRY_END);
+		size++;
 		}
 	
 	printf("\n");
+	printf("after add funcs\n");
+	for(int i = 0; i < 50; i++){
+		printf("%d-",arry[i]);
+		}
+	*/
+	
+	/*
+	int size = 5;
+	int* arry = malloc(sizeof(int)*size);
+	
+	for(int i = 0; i < size; i++){
+		arry[i] = i+1;
+		}
+		
+	printf("Before add funcs\n");
+	for(int i = 0; i < size; i++){
+		printf("%d-",arry[i]);
+		}
+	
+	// deletes the last element 
+	Int_DellElement(&arry,size,ARRY_END);
+	
+	printf("\n");
+	printf("after add funcs\n");
+	for(int i = 0; i < 4; i++){
+		printf("%d-",arry[i]);
+		}
+	
+	*/
+	
+	/*
+	int size = 5;
+	int* arry = malloc(sizeof(int)*size);
+	
+	for(int i = 0; i < size; i++){
+		arry[i] = i+1;
+		}
+		
+	printf("Before add funcs\n");
+	for(int i = 0; i < size; i++){
+		printf("%d-",arry[i]);
+		}
+	
+	// The specified element of the array is deleted.
+	Int_DellElement(&arry,size,3);
+	
+	printf("\n");
+	printf("after add funcs\n");
+	for(int i = 0; i < 4; i++){
+		printf("%d-",arry[i]);
+		}
+	
+	*/
+	
+	return 0;
 
 	}
-	
-	
